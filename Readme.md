4efd06b2f74094bfe8b2f6426a12d06c89a355b2
John Albritton <jtareb1@att.net>
2015-01-23 15:23:28 -0500

    initial commit

--- Readme.md
+++ Readme.md
@@ -6,13 +6,6 @@ You should probably update me so people looking at your code have an idea what i
 
 
 
-<form action="index.html" method="post" id=""/>
-                <input type="text" name="myname" valu="" placeholder="" id="">
-            </form>
 
 
 
-
-$('.contenttitle').append('<p class="contenttitle">' + item.title + '</p>');
-$('.contentprice').append('<p class="contentprice">' + item.price + '</p>');
-$('contentcurr').append('<p class="contentcurr">' + item.currency_code + '</p>');
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,30 @@
 
+
+
+
+
+
+boxcontainer = $('.contentbox');
+
 etsy_items.results.forEach( function (item)	{
 
-$('.contentpage').append('<img src="' + item.Images[0].url_170x135 + ' "/>');
-$('.contenttitle').append('<p class="contenttitle">' + item.title + '</p>');
-$('.contentprice').append('<p class="contentprice">' + item.price + '</p>');
-$('contentcurr').append('<p class="contentcurr">' + item.currency_code + '</p>');
+maincontent = '<div class="item">';
+maincontent += '<img src="' + item.Images[0].url_170x135 + '"/>'; 
+maincontent += '<h4>' + item.title + '</h4>';
+maincontent += '<div class= info>';
+maincontent += '<p class="contentshop">' + item.Shop.shop_name + '</p>';
+maincontent += '<p class="money">$' + item.price + item.currency_code + '</p>';
+maincontent += '</div></div>';
+
+
+	
 
 
 
 
-
-console.log(item.Images);
+boxcontainer.append(maincontent);
 
 });
-
 
 
 
